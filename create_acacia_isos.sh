7a2e0f9972a145d982d338a7709d8ac01da704cd
Max Kofler <kofler.max.dev@gmail.com>
2023-03-31 08:51:29 +0200

    Update creation script

--- create_acacia_isos.sh
+++ create_acacia_isos.sh
@@ -8,13 +8,13 @@ echo "Creating AcaciaLinux ISOs..."
 mkdir -pv $SCRIPT_DIR/builds/{core,core-fw,desk,desk-fw}
 
 create_iso() {
-     ACACIA_ROOT=$SCRIPT_DIR/builds/$1 SQUASHFS=$2 ISO=$3 ISO_PKGS=$4 $SCRIPT_DIR/mkiso.sh
+     ACACIA_ROOT=$SCRIPT_DIR/builds/$1/root SQUASHFS=$SCRIPT_DIR/builds/$1.squash ISO=$3 ISO_PKGS=$4 $SCRIPT_DIR/mkiso.sh
 }
 
-create_iso core core.squash AcaciaLinux-core-$DATE.iso
-create_iso core-fq core-fw.squash AcaciaLinux-core-fw-$DATE.iso "linux-firmware"
+create_iso core AcaciaLinux-core-$DATE.iso
+create_iso core-fq AcaciaLinux-core-fw-$DATE.iso "linux-firmware"
 
-create_iso desk desk.squash AcaciaLinux-desk-$DATE.iso "sway xorg-xinit xorg-fbdev xorg-libinput llvm alacritty"
-create_iso desk-fw desk-fw.squash AcaciaLinux-desk-fw-$DATE.iso "linux-firmware sway xorg-xinit xorg-fbdev xorg-libinput llvm alacritty"
+create_iso desk AcaciaLinux-desk-$DATE.iso "sway xorg-xinit xorg-fbdev xorg-libinput llvm alacritty"
+create_iso desk-fw AcaciaLinux-desk-fw-$DATE.iso "linux-firmware sway xorg-xinit xorg-fbdev xorg-libinput llvm alacritty"
 
 
